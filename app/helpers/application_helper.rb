a159838865a78e558f56a3daf61bf478ecb0209b
Tsarkov Igor <higorusama@gmail.com>
2018-08-30 13:42:28 +0300

    fix places

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,25 +3,25 @@ module ApplicationHelper
   # Выводим имя пользователя и его место, согласна полученым очкам
   def get_player_name(wind, game, players)
     score = [
-      game.score["east"]["player_score"],
-      game.score["south"]["player_score"],
-      game.score["west"]["player_score"],
-      game.score["north"]["player_score"]
+      game.score["east"]["player_score"].to_i,
+      game.score["south"]["player_score"].to_i,
+      game.score["west"]["player_score"].to_i,
+      game.score["north"]["player_score"].to_i
     ]
 
-    if (score.sort[3].to_i == game.score["#{wind}"]["player_score"].to_i) then
+    if score.sort[3].to_i == game.score["#{wind}"]["player_score"].to_i then
       "1. " + players.find(game.score["#{wind}"]["player_id"]).name
 
-    elsif (score.sort[2].to_i == game.score["#{wind}"]["player_score"].to_i)
+    elsif score.sort[2].to_i == game.score["#{wind}"]["player_score"].to_i
       "2. " + players.find(game.score["#{wind}"]["player_id"]).name
 
-    elsif (score.sort[1].to_i == game.score["#{wind}"]["player_score"].to_i)
+    elsif score.sort[1].to_i == game.score["#{wind}"]["player_score"].to_i
       "3. " + players.find(game.score["#{wind}"]["player_id"]).name
 
-    elsif (score.sort[0].to_i == game.score["#{wind}"]["player_score"].to_i)
+    elsif score.sort[0].to_i == game.score["#{wind}"]["player_score"].to_i
       "4. " + players.find(game.score["#{wind}"]["player_id"]).name
     else
-      "!WTF" #players.find(game.score["#{wind}"]["player_id"]).name
+      "!WTF"
     end
   end
 
