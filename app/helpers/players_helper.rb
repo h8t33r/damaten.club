f3c3de4edfb6b94df843b1b1b0ec764a7c932108
Tsarkov Igor <higorusama@gmail.com>
2018-09-03 15:13:34 +0300

    fix results counter

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -13,20 +13,34 @@ module PlayersHelper
         game.score["north"]["player_score"]
       ]
 
-      if p_id == game.score["east"]["player_id"].to_i && game.score["east"]["player_score"].to_i == score.sort[place].to_i then
-        result_array << game.score["east"]["player_score"]
+      if p_id == game.score["east"]["player_id"].to_i &&
+      game.score["east"]["player_score"].to_i == score.sort[place].to_i &&
+      game.score["east"]["player_score"].to_i != score.sort[place+1].to_i then
 
-      elsif p_id == game.score["south"]["player_id"].to_i && game.score["south"]["player_score"].to_i == score.sort[place].to_i
-        result_array << game.score["south"]["player_score"]
+        result_array << game.score["east"]["player_score"].to_i
 
-      elsif p_id == game.score["west"]["player_id"].to_i && game.score["west"]["player_score"].to_i == score.sort[place].to_i
-        result_array << game.score["west"]["player_score"]
+      elsif p_id == game.score["south"]["player_id"].to_i &&
+      game.score["south"]["player_score"].to_i == score.sort[place].to_i &&
+      game.score["south"]["player_score"].to_i != score.sort[place+1].to_i 
 
-      elsif p_id == game.score["north"]["player_id"].to_i && game.score["north"]["player_score"].to_i == score.sort[place].to_i
-        result_array << game.score["north"]["player_score"]       
+        result_array << game.score["south"]["player_score"].to_i
+
+      elsif p_id == game.score["west"]["player_id"].to_i &&
+      game.score["west"]["player_score"].to_i == score.sort[place].to_i &&
+      game.score["west"]["player_score"].to_i != score.sort[place+1].to_i
+
+        result_array << game.score["west"]["player_score"].to_i
+
+      elsif p_id == game.score["north"]["player_id"].to_i &&
+      game.score["north"]["player_score"].to_i == score.sort[place].to_i &&
+      game.score["north"]["player_score"].to_i != score.sort[place+1].to_i 
+
+        result_array << game.score["north"]["player_score"].to_i
+      else
+        # не делаем ничего
       end
     end
     
-    render html: result_array.count
+    return result_array
   end
 end
